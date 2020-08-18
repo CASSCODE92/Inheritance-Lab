9fabb61229afdb28b268661a9abfac17f6c88051
CASSCODE92 <67381271+CASSCODE92@users.noreply.github.com>
2020-08-18 03:54:28 -0400

    Create inheritance.java

---
+++ inheritance.java
@@ -0,0 +1,110 @@
+class Account {
+    String accountType;
+    String name;
+    double bal;
+    String pin;
+    double minBal;
+    double penalty;
+    double fee;
+    int trans;
+    int months;
+    void chrgPenalty() = while (bal < minBal) {
+        double bal = bal - penalty;
+    }
+}
+
+Account Joe = new Account();
+    String Joe.accountType = "Regular Account"
+    String Joe.name = "Joe";
+    double Joe.bal = 900.00;
+    String Joe.pin = "0123";
+    double Joe.minBal = 500.00;
+    double Joe.penalty = Joe.bal - 10.00;
+    double Joe.fee = Joe.bal - 10.00;
+
+Account Jane = new Account();
+    String Jane.accountType = "Interest Account"
+    String Jane.name = "Joe";
+    double Jane.bal = 1300.00;
+    String Jane.pin = "1234";
+    double Jane.fee = Jane.bal - 10.00;
+    double interest = Jane.bal - (Jane.bal * 0.07);
+
+Account Jill = new Account();
+    String Jill.accountType = "Checking Account"
+    String Jill.name = "Jill";
+    double Jill.bal = 2100.00;
+    String Jill.pin = "2311";
+    double Jill.minBal = 100.00;
+    double Jill.penalty = Jill.bal - 10;
+    double Jill.fee = Jill.bal - 10.00;
+    double Jill.interest = Jill.bal - (Jill.bal * 0.07);
+    int Jill.trans = ; 
+
+Account Jack = new Account();
+    String Jack.accountType = "CD Account"
+    String Jack.name = "Jack";
+    double Jack.bal = 2100.00;
+    String Jack.pin = "0023";
+    double Jack.penalty = Jill.bal - 10;
+    double Jack.interest = Jack.bal - (Jack.bal * 0.07);
+    int Jack.months = 0;
+
+
+Cedric Etheridge
+2:45 AM (1 hour ago)
+to me
+
+class Account {
+    String accountType;
+    String name;
+    double bal;
+    String pin;
+    double minBal;
+    double penalty;
+    double fee;
+    int trans;
+    int months;
+    double interest;
+    public void chrgPenalty() { if (bal < minBal) {
+        double bal = bal - penalty;
+        }
+    }
+}
+
+Account Joe = new Account();
+    String Joe.accountType = "Regular Account"
+    String Joe.name = "Joe";
+    double Joe.bal = 900.00;
+    String Joe.pin = "0123";
+    double Joe.minBal = 500.00;
+    double Joe.penalty = Joe.bal - 10.00;
+    double Joe.fee = Joe.bal - 10.00;
+
+Account Jane = new Account();
+    String Jane.accountType = "Interest Account"
+    String Jane.name = "Joe";
+    double Jane.bal = 1300.00;
+    String Jane.pin = "1234";
+    double Jane.fee = Jane.bal - 10.00;
+    double Jane.interest = Jane.bal - (Jane.bal * 0.07);
+
+Account Jill = new Account();
+    String Jill.accountType = "Checking Account"
+    String Jill.name = "Jill";
+    double Jill.bal = 2100.00;
+    String Jill.pin = "2311";
+    double Jill.minBal = 100.00;
+    double Jill.penalty = Jill.bal - 10;
+    double Jill.fee = Jill.bal - 10.00;
+    double Jill.interest = Jill.bal - (Jill.bal * 0.07);
+    int Jill.trans = ; 
+
+Account Jack = new Account();
+    String Jack.accountType = "CD Account"
+    String Jack.name = "Jack";
+    double Jack.bal = 2100.00;
+    String Jack.pin = "0023";
+    double Jack.penalty = Jill.bal - 10;
+    double Jack.interest = Jack.bal - (Jack.bal * 0.07);
+    int Jack.months = 0;
